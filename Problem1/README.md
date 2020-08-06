1b6febcb73d6f385c5785e1e82041e795d9ccd10
TipsByPullak <pullak10@gmail.com>
2020-08-06 14:32:32 +0530

    First commit after writitng and compiling the program

---
+++ Problem1/README.md
@@ -0,0 +1,7 @@
+# Gophercises Problem 1- My solution
+###### Link to problem statement- [Problem 1 Overview](https://courses.calhoun.io/lessons/les_goph_01)
+
+### Build instructions
+1. In your terminal, navigate to directory "Problem 1" and type- `go build`
+2. You will find a default quiz csv as "problems.csv". But if you want to use a custom csv file, use the -filename flag, by typing `./a -filename <your csv file name>` (Assuming you didn't use the -o flag during `go build` in step 2)
+3. Follow the instructions and answer the questions accordingly.
---
+++ Problem1/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+//Struct to track the stats of the quiz
+type quizStat struct {
+	noOfCorrect int
+	noOfTotal   int
+}
+
+func main() {
+	//Read the flag and parse it
+	filePtr := flag.String("filename", "problems.csv", "Used to provide the filename to the executable")
+	flag.Parse()
+
+	//Testing the flag parser-
+	//fmt.Println(*filePtr)
+
+	//Create the iterator to read each question and answer
+	problemReader := csvReader(*filePtr)
+
+	//Start the Quiz and print stats at the end
+	stat := startQuiz(problemReader)
+	fmt.Printf("\n-----QUIZ END-----.\nNo. of correct answers= %v\nNo. of Questions Attempted= %v\n\n", stat.noOfCorrect, stat.noOfTotal)
+}
+
+//Function to read the CSV file
+func csvReader(filename string) *csv.Reader {
+	file, err := os.Open(filename) //Open the file
+	//If file opening fails
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(2)
+	}
+
+	return csv.NewReader(file)
+}
+
+func startQuiz(reader *csv.Reader) quizStat {
+	stat := quizStat{} //the struct to be returned
+
+	//Quiz only starts once the user presses enter
+	fmt.Println("Press Enter to start the quiz")
+	fmt.Scanln()
+
+	var answer string
+	for {
+		question, err := reader.Read() //Read the next question
+		if err == io.EOF {             //If all questions have been read
+			break
+		}
+		if err != nil { //If there's an error in reading the file
+			fmt.Println("Error:", err)
+		}
+
+		(stat.noOfTotal)++                    //Total number of questions attempted is increased
+		fmt.Printf("Q: %v\nA: ", question[0]) //Display the question
+		fmt.Scanln(&answer)                   //Read the answer //Implement string trimming
+		if answer == question[1] {
+			(stat.noOfCorrect)++ //Increase number of correct answers if answered correctly
+		}
+	}
+
+	return stat
+}
