703818dae24c9a3354c531c9d5b9d6b82dfd5be3
TipsByPullak <pullak10@gmail.com>
2020-08-08 13:14:41 +0530

    Completed do command for now

--- Problem3/task/cmd/do.go
+++ Problem3/task/cmd/do.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strconv"
+	"task/db"
 
 	"github.com/spf13/cobra"
 )
@@ -18,9 +20,28 @@ var doCmd = &cobra.Command{
 			if err != nil {
 				fmt.Println("Failed to parse", arg)
 			}
-			ids = append(ids, id)
+			ids = append(ids, id) //Get all the ids to be marked done
 		}
-		fmt.Println(ids)
+		tasks, err := db.AllTasks() //Fetch a list of all tasks
+		if err != nil {
+			fmt.Println("Something went wrong while removing:", err)
+			os.Exit(1)
+		}
+
+		for _, id := range ids { //iterate over the tasks
+			if id <= 0 || id > len(tasks) { //If invalid imput provided
+				fmt.Println("Invalid task number:", id)
+				continue
+			}
+			task := tasks[id-1]             //Remember, list printed is 1-based, code is 0-based
+			dErr := db.DeleteTask(task.Key) //Delete the task from the DB
+			if dErr != nil {
+				fmt.Printf("Failed to mark \"%d\" as completed. Error: %s\n", id, err)
+			} else {
+				fmt.Printf("Marked \"%d\" as completed\n", id)
+			}
+		}
+		// fmt.Println(ids)
 	},
 }
 
