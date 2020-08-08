262db0c255466acb5eab8211093484d4c0788826
TipsByPullak <pullak10@gmail.com>
2020-08-08 13:28:09 +0530

    Renamed Problem3 folder to Problem7 and added a README for Problem7

--- Problem3/task/cmd/root.go
+++
@@ -1,9 +0,0 @@
-package cmd
-
-import "github.com/spf13/cobra"
-
-//RootCmd is the code to show the help list while running task in the CLI
-var RootCmd = &cobra.Command{
-	Use:   "task",
-	Short: "task is your rudimentary CLI TODO manager",
-}
